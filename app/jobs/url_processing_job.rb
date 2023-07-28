cd25f603ec3f712880bd66fb5a30b1949047d23e
Nabeel Shafiq <engr.nabeelshafiq@gmail.com>
2023-07-28 18:30:22 -0230

    Create User Interface and configure robocob

--- app/jobs/url_processing_job.rb
+++ app/jobs/url_processing_job.rb
@@ -4,12 +4,14 @@ class UrlProcessingJob < ApplicationJob
   queue_as :default
 
   def perform(url)
-    doc = Nokogiri::HTML(open(url))
-    image_tag = doc.at('meta[property="og:image"]')
+    preview = Preview.find_by(url: url)
 
-    image_url = image_tag['content'] if image_tag
-
-    preview = Preview.create(url: url, image_url: image_url)
+    unless preview
+      doc = Nokogiri::HTML(open(url))
+      image_tag = doc.at('meta[property="og:image"]')
+      image_url = image_tag['content'] if image_tag
+      preview = Preview.create(url: url, image_url: image_url)
+    end
     ActionCable.server.broadcast("preview_channel", preview)
   rescue StandardError
     nil
